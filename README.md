9d7b58447850bb619bc5b81307938edec169e264
mzamani <mzamani@cs.stonybrook.edu>
2017-04-17 18:54:54 -0400

    gitignore

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # real-estate
+
+virtualenv --system-site-packages venv
+source ~/venv/bin/activate
+pip install --upgrade tensorflow
+
+pip install pandas
+
+To add an existing virtual environment to the list of available interpreters:
+    In the Project Interpreter page, click cogwheel (settings).
+    In the drop-down list, choose Add local.
+    In the Select Python Interpreter dialog box that opens, choose the desired Python executable, located inside the virtual environment folder, and click OK
--- util.py
+++ util.py
@@ -2,36 +2,49 @@ import numpy as np
 import pandas as pd
 import MySQLdb
 import sys
+from db_info import db_info
 
 from keras.layers import Dense, Activation
 from keras.models import Sequential
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 from keras.layers import Dense, Dropout, Activation, Embedding, LSTM, SimpleRNN, GRU, Merge
 
+
+
 class util:
 
-    def connectMysqlDB(self):
-                conn = MySQLdb.connect(self.host, self.user, self.password, self.database)
+    feat_table = 'features'
+
+    def main(self):
+        my_db_info = db_info()
+        data_frame = self.retrieve_input(my_db_info)
+        # for index , row in data_frame.iterrows():
+        #     print index, ' , ', row
+
+
+    def connectMysqlDB(self, db_info):
+                conn = MySQLdb.connect(db_info.host, db_info.user, db_info.password, db_info.database)
                 c = conn.cursor()
                 return c
 
 
-    def retrieve_input(self):
+    def retrieve_input(self, db_info):
                 print 'retrieve_input'
                 data = []
                 columns = []
                 try:
-                        self.cursor = self.connectMysqlDB()
+                        self.cursor = self.connectMysqlDB(db_info)
                 except:
                         print("error while connecting to database:", sys.exc_info()[0])
                         raise
                 if(self.cursor is not None):
-                                sql = "show columns from {0}".format(self.trust_table)
+                                sql = "show columns from {0}".format(self.feat_table)
+                                print 'sql: ' , sql
                                 self.cursor.execute(sql)
                                 columns_name = self.cursor.fetchall()
                                 for row in columns_name:
                                         columns.append(row[0])
-                                sql = "select * from {0}".format(self.trust_table)
+                                sql = "select * from {0}".format(self.feat_table)
                                 self.cursor.execute(sql)
                                 result = self.cursor.fetchall()
                                 for row in result:
@@ -39,5 +52,8 @@ class util:
 
                 print 'len(data):  '  , len(data) , '  ' ,  data[0]
                 data_frame = pd.DataFrame(data = data, columns = columns)
-                #trust_data_frame = trust_data_frame.set_index('user_id')
                 return data_frame
+
+
+utility = util()
+utility.main()
