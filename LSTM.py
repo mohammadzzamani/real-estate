1b1e30c8772d6373d058ee8096ad4f721ab85f4b
mzamani <mzamani@cs.stonybrook.edu>
2017-04-25 18:38:48 -0400

    .

--- LSTM.py
+++ LSTM.py
@@ -156,9 +156,9 @@ def get_train_and_test(dataset, train_size):
     # trainX, trainY = create_dataset(train, look_back)
     # testX, testY = create_dataset(test, look_back)
 
-    trainX = trainX[0:2000, :]
+    trainX = trainX[2000:4000, :]
     testX = testX[0:100, :]
-    trainY = trainY[0:2000]
+    trainY = trainY[2000:4000]
     testY = testY[0:100]
 
     print "TrainX: ", trainX.shape
