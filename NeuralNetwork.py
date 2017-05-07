b0e84c4063d120ec5fe7cc87434cbfb72b948e73
mzamani <mzamani@cs.stonybrook.edu>
2017-05-07 14:15:04 -0400

    NeuralNetwork.py

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -219,33 +219,8 @@ class NeuralNetwork_:
 
 
 
-    def linear_model(self, xTrain, yTrain, xTest, yTest):
-        lr = linear_model.LinearRegression()
-        lr.fit(xTrain, yTrain)
-        lr_pred_test = lr.predict(xTest)
-        lr_pred_train = lr.predict(xTrain)
-
-        print 'Result_test: ', mean_squared_error(yTest, lr_pred_test)
-        print 'Result_train: ', mean_squared_error(yTrain, lr_pred_train)
-
-        print 'Result_test: ', mean_absolute_error(yTest, lr_pred_test)
-        print 'Result_train: ', mean_absolute_error(yTrain, lr_pred_train)
-
-        lr_pred_test = np.sign(lr_pred_test)
-        lr_pred_train = np.sign(lr_pred_train )
-
-        yTest_c =  np.sign(yTest)
-        yTrain_c = np.sign(yTrain)
-
-        print ' lr test accuracy: ' , sum(1 for x,y in zip(np.sign(lr_pred_test),np.sign(yTest)) if x == y) / float(len(yTest))
-        print ' lr train accuracy: ' , sum(1 for x,y in zip(np.sign(lr_pred_train),np.sign(yTrain)) if x == y) / float(len(yTrain))
-
-        print  'lr.coef_: '
-        print lr.coef_
-
-
-    def linear_classifier(self, train_set, test_set):
-
+    def linear_model(self, train_set, test_set):
+        print ' linear_model '
         # yTrain = np.sign(yTrain)
         # ytest = np.sign(yTest)
 
@@ -254,9 +229,6 @@ class NeuralNetwork_:
         yTrain = train_set.ix[:,-1].values
         xTest = test_set.ix[:, :-1].values
         yTest = test_set.ix[:,-1].values
-
-        # xPred = train_set.label_prev.ix[:].values
-        # yPred = test_set.label_prev.ix[:].values
 
         yPrevTest = test_set.ix[:, NUM_FEATURES].values
         yPrevTrain = train_set.ix[:,NUM_FEATURES].values
@@ -283,6 +255,57 @@ class NeuralNetwork_:
 
         print  'lr.coef_: '
         print lr.coef_
+
+
+    def linear_classifier(self, train_set, test_set):
+        print ' linear_classifier '
+        # yTrain = np.sign(yTrain)
+        # ytest = np.sign(yTest)
+
+
+        xTrain = train_set.ix[:, :-1].values
+        yTrain = train_set.ix[:,-1].values
+        xTest = test_set.ix[:, :-1].values
+        yTest = test_set.ix[:,-1].values
+
+        # xPred = train_set.label_prev.ix[:].values
+        # yPred = test_set.label_prev.ix[:].values
+
+        yPrevTest = test_set.ix[:, NUM_FEATURES].values
+        yPrevTrain = train_set.ix[:,NUM_FEATURES].values
+
+
+        yTest = np.sign(yTest - yPrevTest)
+        yTrain = np.sign(yTrain - yPrevTrain)
+
+        # lr = linear_model.LinearRegression()
+        clf = linear_model.SGDClassifier()
+        clf.fit(xTrain, yTrain)
+        clf_pred_test = clf.predict(xTest)
+        clf_pred_train = clf.predict(xTrain)
+
+        print 'examples:'
+        print yTest[:10]
+        print clf_pred_test[:10]
+
+        print 'Result_test: ', mean_squared_error(yTest, clf_pred_test)
+        print 'Result_train: ', mean_squared_error(yTrain, clf_pred_train)
+
+        print 'Result_test: ', mean_absolute_error(yTest, clf_pred_test)
+        print 'Result_train: ', mean_absolute_error(yTrain, clf_pred_train)
+
+        print ' clf test accuracy: ' , sum(1 for x,y in zip(clf_pred_test,yTest ) if x == y) / float(len(yTest))
+        print ' clf train accuracy: ' , sum(1 for x,y in zip(clf_pred_train,yTrain ) if x == y) / float(len(yTrain))
+
+
+        clf_pred_test = np.sign(clf_pred_test - yPrevTest)
+        clf_pred_train = np.sign(clf_pred_train - yPrevTrain )
+
+        # yTest_c =  np.sign(yTest)
+        # yTrain_c = np.sign(yTrain)
+
+        print  'clf.coef_: '
+        print clf.coef_
 
 
     def __init__(self):
@@ -373,7 +396,7 @@ if __name__ == "__main__":
 
 
     #linear regression
-    Network.linear_model( xTrain, yTrain, xTest, yTest)
+    Network.linear_model( train_set, test_set)
     Network.linear_classifier( train_set, test_set)
 
     '''
