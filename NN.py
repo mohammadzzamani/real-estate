c055b3ff0d4aabcd5294eb459a72185a02fe2188
mzamani <mzamani@cs.stonybrook.edu>
2017-05-12 00:20:53 -0400

    NN.py

--- NN.py
+++ NN.py
@@ -107,13 +107,13 @@ class NN:
         # yTrain = np.sign(yTrain - yPrevTrain) #.
         # create model
         model = Sequential()
-        model.add(Dense(30, input_dim=len(xTrain[0]) , init='normal', activation='relu'))  #*
+        model.add(Dense(30, input_dim=len(xTrain[0]) , init='normal', activation='tanh'))  #*
         # model.add(Dense(20, input_dim=len(xTrain[0]) , init='normal', activation='tanh'))  #.
         # model.add(layers.core.Dropout(0.2))
         # model.add(Dense(output_dim = 30, init='normal' , activation = 'relu'))
         # model.add(layers.core.Dropout(0.2))
         # model.add(Dense(output_dim = 10, init='normal' , activation = 'relu'))
-        model.add(layers.core.Dropout(0.2))
+        model.add(layers.core.Dropout(0.1))
         model.add(Dense(output_dim = 5, init='normal' , activation = 'linear'))  #*
         # model.add(Dense(output_dim = 5, init='normal' , activation = 'linear'))  #.
         # model.add(Dense(1, init='normal', activation= 'softmax')) #.
