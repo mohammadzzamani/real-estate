679e2d3f89d9541d541becab8fa5c989b07c0081
mzamani <mzamani@cs.stonybrook.edu>
2017-05-11 23:38:00 -0400

    NN.py

--- NN.py
+++ NN.py
@@ -103,26 +103,27 @@ class NN:
 
         print 'yPrevTest'
         print yPrevTest
-        # yTest = np.sign(yTest - yPrevTest)
-        # yTrain = np.sign(yTrain - yPrevTrain)
+        yTest = np.sign(yTest - yPrevTest)
+        yTrain = np.sign(yTrain - yPrevTrain)
         # create model
         model = Sequential()
+        # model.add(Dense(30, input_dim=len(xTrain[0]) , init='normal', activation='relu'))  #*
         model.add(Dense(30, input_dim=len(xTrain[0]) , init='normal', activation='relu'))
         # model.add(layers.core.Dropout(0.2))
         # model.add(Dense(output_dim = 30, init='normal' , activation = 'relu'))
         # model.add(layers.core.Dropout(0.2))
         # model.add(Dense(output_dim = 10, init='normal' , activation = 'relu'))
         # model.add(layers.core.Dropout(0.2))
-        model.add(Dense(output_dim = 5, init='normal' , activation = 'linear'))
-        # model.add(Dense(1, init='normal', activation= 'softmax'))
+        # model.add(Dense(output_dim = 5, init='normal' , activation = 'linear'))  #*
+        model.add(Dense(1, init='normal', activation= 'softmax'))
         model.add(Dense(1, init='normal'))
         # Compile model
 
         lr = 0.05
         decay = 0.975
         adam = optimizers.adam(lr = lr, decay = decay)
-        # model.compile(loss = 'binary_crossentropy', optimizer = adam)
-        model.compile(loss = 'mean_absolute_error', optimizer = adam)
+        model.compile(loss = 'binary_crossentropy', optimizer = adam)
+        # model.compile(loss = 'mean_absolute_error', optimizer = adam)  #*
         nb_epochs = 500
         for i in xrange(nb_epochs):
 
@@ -136,10 +137,10 @@ class NN:
                 testPredict = model.predict(xTest, verbose = 1)
                 print 'Neural Network_i: ', i , ' , ' ,  lr , ' , ' ,   mean_squared_error(yTest, testPredict)
                 print 'Neural Network_i: ', i , ' , ', mean_absolute_error(yTest, testPredict)
-                # print 'Neural Network accuracy: ' , sum(1 for x,y in zip(testPredict,yTest ) if x == y) / float(len(yTest))
+                print 'Neural Network accuracy: ' , sum(1 for x,y in zip(testPredict,yTest ) if x == y) / float(len(yTest))
 
-                testPredict = testPredict.reshape(testPredict.shape[0])
-
+                # testPredict = testPredict.reshape(testPredict.shape[0])  #*
+                # print ' test accuracy: ' , sum(1 for x,y in zip(np.sign(testPredict - yPrevTest),np.sign(yTest- yPrevTest)) if x == y) / float(len(yTest))  #*
                 # print 'yPrevTest'
                 # print yPrevTest
                 # x1 = np.sign(testPredict - yPrevTest)
@@ -159,7 +160,7 @@ class NN:
                 # print s2[:100]
 
                 # print ' accuracy: ' , mean_absolute_error(x1, x2)
-                print ' test accuracy: ' , sum(1 for x,y in zip(np.sign(testPredict - yPrevTest),np.sign(yTest- yPrevTest)) if x == y) / float(len(yTest))
+
                 # print ' lr test accuracy: ' , sum(1 for x,y in zip(np.sign(lr_pred_test - yPrevTest),np.sign(yTest - yPrevTest)) if x == y) / float(len(yTest))
 
     # Build the neural network
