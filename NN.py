b4decfb8edfb5d8decad5d2c4aa7d9dc9bb97f0c
mzamani <mzamani@cs.stonybrook.edu>
2017-05-11 23:41:41 -0400

    NN.py

--- NN.py
+++ NN.py
@@ -108,13 +108,14 @@ class NN:
         # create model
         model = Sequential()
         # model.add(Dense(30, input_dim=len(xTrain[0]) , init='normal', activation='relu'))  #*
-        model.add(Dense(30, input_dim=len(xTrain[0]) , init='normal', activation='linear'))
+        model.add(Dense(20, input_dim=len(xTrain[0]) , init='normal', activation='tanh'))
         # model.add(layers.core.Dropout(0.2))
         # model.add(Dense(output_dim = 30, init='normal' , activation = 'relu'))
         # model.add(layers.core.Dropout(0.2))
         # model.add(Dense(output_dim = 10, init='normal' , activation = 'relu'))
         # model.add(layers.core.Dropout(0.2))
         # model.add(Dense(output_dim = 5, init='normal' , activation = 'linear'))  #*
+        model.add(Dense(output_dim = 5, init='normal' , activation = 'linear'))
         model.add(Dense(1, init='normal', activation= 'softmax'))
         # model.add(Dense(1, init='normal'))
         # Compile model
