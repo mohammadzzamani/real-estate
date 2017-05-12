a78c166c7745a5161021e94516da1914987d7e35
mzamani <mzamani@cs.stonybrook.edu>
2017-05-12 11:59:03 -0400

    NN.py

--- NN.py
+++ NN.py
@@ -76,14 +76,14 @@ class NN:
         print new_df.columns
 
         train_df.drop('cnty', axis=1, inplace=True)
-        train_df.drop('month', axis=1, inplace=True)
+        # train_df.drop('month', axis=1, inplace=True)
         # train_df.drop('cnty_prev', axis=1, inplace=True)
         # train_df.drop('month_prev', axis=1, inplace=True)
         # train_df.drop('cnty_prev_2', axis=1, inplace=True)
         # train_df.drop('month_prev_2', axis=1, inplace=True)
 
         test_df.drop('cnty', axis=1, inplace=True)
-        test_df.drop('month', axis=1, inplace=True)
+        # test_df.drop('month', axis=1, inplace=True)
         # test_df.drop('cnty_prev', axis=1, inplace=True)
         # test_df.drop('month_prev', axis=1, inplace=True)
         # test_df.drop('cnty_prev_2', axis=1, inplace=True)
