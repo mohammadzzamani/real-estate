10d9732f5748a632724bbdb71a6cbfd41fbe3680
mzamani <mzamani@cs.stonybrook.edu>
2017-05-11 23:03:20 -0400

    NN.py

--- NN.py
+++ NN.py
@@ -75,19 +75,19 @@ class NN:
         train_df = new_df[new_df.month <= train_month]
         print new_df.columns
 
-        # train_df.drop('cnty', axis=1, inplace=True)
-        # train_df.drop('month', axis=1, inplace=True)
-        # train_df.drop('cnty_prev', axis=1, inplace=True)
-        # train_df.drop('month_prev', axis=1, inplace=True)
-        # train_df.drop('cnty_prev_2', axis=1, inplace=True)
-        # train_df.drop('month_prev_2', axis=1, inplace=True)
+        train_df.drop('cnty', axis=1, inplace=True)
+        train_df.drop('month', axis=1, inplace=True)
+        train_df.drop('cnty_prev', axis=1, inplace=True)
+        train_df.drop('month_prev', axis=1, inplace=True)
+        train_df.drop('cnty_prev_2', axis=1, inplace=True)
+        train_df.drop('month_prev_2', axis=1, inplace=True)
 
-        # test_df.drop('cnty', axis=1, inplace=True)
-        # test_df.drop('month', axis=1, inplace=True)
-        # test_df.drop('cnty_prev', axis=1, inplace=True)
-        # test_df.drop('month_prev', axis=1, inplace=True)
-        # test_df.drop('cnty_prev_2', axis=1, inplace=True)
-        # test_df.drop('month_prev_2', axis=1, inplace=True)
+        test_df.drop('cnty', axis=1, inplace=True)
+        test_df.drop('month', axis=1, inplace=True)
+        test_df.drop('cnty_prev', axis=1, inplace=True)
+        test_df.drop('month_prev', axis=1, inplace=True)
+        test_df.drop('cnty_prev_2', axis=1, inplace=True)
+        test_df.drop('month_prev_2', axis=1, inplace=True)
 
         print 'train_df.shape: ' , train_df.shape
         print 'test_df.shape: ' ,test_df.shape
